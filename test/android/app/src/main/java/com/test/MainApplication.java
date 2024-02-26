f0bfe09a29b76e4a04f3289e354e44594c80fde8
Calvin Chen <cchen1872@gmail.com>
2024-02-25 20:47:52 -0500

    Revert "vector icons commit"
    This reverts commit 6aa00a626cf69828928038fbbe9ac148147047de.

--- test/android/app/src/main/java/com/test/MainApplication.java
+++ test/android/app/src/main/java/com/test/MainApplication.java
@@ -18,8 +18,6 @@ import expo.modules.ReactNativeHostWrapper;
 
 import java.util.List;
 
-import com.oblador.vectoricons.VectorIconsPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -34,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
         @SuppressWarnings("UnnecessaryLocalVariable")
         List<ReactPackage> packages = new PackageList(this).getPackages();
         // Packages that cannot be autolinked yet can be added manually here, for example:
-        //packages.add(new MyReactNativePackage());
+        // packages.add(new MyReactNativePackage());
         return packages;
       }
 
