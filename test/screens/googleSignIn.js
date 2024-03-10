cea7fa5bfa5db2d92378d8c35390b85758af1faf
nashrazaman <nz2065@nyu.edu>
2024-03-10 17:16:30 -0400

    started google auth

---
+++ test/screens/googleSignIn.js
@@ -0,0 +1,19 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import {
+	GOOGLE_WEB_CLIENT_ID,
+	GOOGLE_ANDROID_CLIENT_ID,
+	GOOGLE_IOS_CLIENT_ID,
+} from '@env';
+
+GoogleSignin.configure({
+	webClientId: GOOGLE_WEB_CLIENT_ID,
+	androidClientId: GOOGLE_ANDROID_CLIENT_ID,
+	iosClientId: GOOGLE_IOS_CLIENT_ID,
+	scopes: ['profile', 'email'],
+});
+
+const GoogleLogin = async () => {
+	await GoogleSignin.hasPlayServices();
+	const userInfo = await GoogleSignin.signIn();
+	return userInfo;
+};
